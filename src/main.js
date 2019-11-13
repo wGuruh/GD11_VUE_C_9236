36d1e93f2da8e2aa1faea2cce03a1e02cf6664cb
wGuruh <wicaksana025@gmail.com>
2019-11-14 00:08:29 +0700

    my first commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App'
+import Axios from'axios'
+import router from'./router'
+import vuetify from './plugins/vuetify';
+
+Vue.prototype.$http=Axios;
+Vue.prototype.$apiUrl='http://127.0.0.1:8080/modul11/index.php';
+
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  render: h => h(App),
+  router,
+  components: { App }
+}).$mount('#app')
